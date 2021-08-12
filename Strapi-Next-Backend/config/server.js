f65e08fcf3db9b89d1d4276e5fe7a2f2ea46263f
daniel1890 <danielroth1890@gmail.com>
2021-08-12 15:55:00 +0200

    start project

---
+++ Strapi-Next-Backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b65e2e45a3ac1128aeaf36237966f452'),
+    },
+  },
+});
---
+++ Strapi-Next-Backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '267c146a-0c02-404e-b89d-54a8d45edcdb'
+};
